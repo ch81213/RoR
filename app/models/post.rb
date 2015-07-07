fad4367c9c3956cd8905a386dc0188d6f46b226d
ch81213 <ch81213@gmail.com>
2015-07-07 15:24:04 +0800

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+	validate :content , presence: true
+	belongs_to :group ,counter_cache: :posts_count
+  	belongs_to :author, class_name: "User", foreign_key: :user_id
+	def editable_by?(user)
+	   user && user == author
+	end
+end
